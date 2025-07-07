f68897789f5800d9abe392447ac09c2ffff99c29
ryota37 <ryota.nakahama@access-company.com>
2025-07-07 19:35:50 +0900

    Impl: a circle that diminishes when left clicked

--- ClickGame/Main.cpp
+++ ClickGame/Main.cpp
@@ -2,9 +2,12 @@
 
 void Main()
 {
+	Circle circle{50,50,50};
+	bool isAlive = true;
 
 	while (System::Update())
 	{
-
+		if(isAlive) circle.draw();
+		if (circle.leftClicked()) isAlive = false;
 	}
 }
